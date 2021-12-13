bc00066e2ffe09b029b0a2b2853539ee5f1123ea
Yalegama <nadunyalegama@gmai.com>
2021-12-13 13:48:13 +0530

    7th commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar } from '@material-ui/core'
+import { AppBar, Box, FormControl, InputLabel, MenuItem, Select, Toolbar, Typography} from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles';
 import React from 'react'
 
@@ -7,7 +7,31 @@ const useStyles=makeStyles((theme)=>({
     appBar:{
         backgroundColor:'#00008B',
         position:'static',
+
+    },
+    toolBar:{
+        justifyContent:'space-between',
+    },
+    appHeader:{
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'space-between',
         
+    },
+    logoText:{
+        color:'yellow',
+        fontWeight:'bold',
+        cursor:'pointer'
+    },
+    formControl:{
+        backgroundColor:'white',
+        width:200,
+        borderRadius:10,
+    },select:{
+        height:35
+    },
+    inputLabel:{
+        marginLeft:70,
     }
 }))
 
@@ -16,8 +40,25 @@ function Header() {
     const classes=useStyles();
     return (
         <AppBar className={classes.appBar}>
-            <Toolbar>
-                This is will be Header
+            <Toolbar className={classes.toolBar}>
+                <Typography
+                className={classes.logoText}
+                variant='h4'
+                >
+                    Covid 19 Tracker
+                </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel className={classes.inputLabel}>Country</InputLabel>
+        <Select
+          className={classes.select}
+          label="Age"
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    
             </Toolbar>
         </AppBar>
     )
