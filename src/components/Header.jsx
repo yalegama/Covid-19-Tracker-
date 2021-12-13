c844aed40d396d057e59f3aadc8dfe3b2f2c6c0e
Yalegama <nadunyalegama@gmai.com>
2021-12-13 12:46:19 +0530

    create homaePage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
 import { AppBar, Toolbar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles';
 import React from 'react'
 
+
+const useStyles=makeStyles((theme)=>({
+    appBar:{
+        backgroundColor:'#00008B',
+        position:'static',
+        
+    }
+}))
+
+
 function Header() {
+    const classes=useStyles();
     return (
-        <AppBar>
+        <AppBar className={classes.appBar}>
             <Toolbar>
-                
+                This is will be Header
             </Toolbar>
         </AppBar>
     )
